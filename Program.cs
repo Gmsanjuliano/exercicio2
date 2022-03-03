91dd5534391fd3957814ea07125acbf60d126357
gmsanjuliano <gmsanjuliano@gmail.com>
2022-03-02 21:44:24 -0300

    innitial-commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C_
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Console.WriteLine("Indorme o valor em USD$: ");
+      string valorDigitado = Console.ReadLine();
+      decimal valorConvertido = 0.0m, valorCambio = 5.22m;
+
+      decimal.TryParse(valorDigitado, out valorConvertido);
+
+      decimal valorCambioConvertido = valorConvertido * valorCambio;
+
+      Console.WriteLine($"valor em $: {valorCambioConvertido}");
+      Console.ReadLine();
+    }
+  }
+}
